4eac2ea9b9b669d19766784327521dd7c523d640
francoisledroff <francois.le.droff@gmail.com>
2014-06-13 17:13:59 +0200

    starting aem cookbook testing

--- Berksfile
+++ Berksfile
@@ -3,6 +3,7 @@ source "https://api.berkshelf.com"
 cookbook 'ntp', '~> 1.5.4'
 cookbook 'chef-client', '~> 3.2.0'
 
+cookbook 'aem', path: '~/workspace/github/aem-cookbook/'
  
 
 
--- environments/dev.rb
+++ environments/dev.rb
@@ -1,6 +1,7 @@
 name "dev"
 description "Environment for dev nodes"
 override_attributes({
+  "dev_mode" => "true", #chef-vault-util dev mode will use plain data_bags and not encrypted
   "cq5" => {"vault_accounts_cq_admin_item" => "dev_cq_admin" },
 }) 
 
